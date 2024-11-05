4162e7f7c46718921476fcc94da212266419a369
Pettoo10 <peter.beraxa@gmail.com>
2024-11-05 19:04:02 +0100

    Change sqlite to postgresql db

--- forecast/admin.py
+++ forecast/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import WeatherArticle, WeatherForecast
+
 # Register your models here.
+admin.site.register(WeatherForecast)
+admin.site.register(WeatherArticle)
--- weather_app/urls.py
+++ weather_app/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/forecast/', include('forecast.urls')),  # Pridáme cesty pre forecast API
+    path("admin/", admin.site.urls),
+    path("api/forecast/", include("forecast.urls")),  # Add route for forecast API
 ]
